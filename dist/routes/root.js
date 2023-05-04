79d498347c1b948f5a2a73d5fe42b8144a51e43c
Melnik <supermadmel@gmail.com>
2023-05-04 14:32:09 +1000

    first commit

--- dist/routes/root.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const rootController_ts_1 = require("../controllers/rootController.ts");
-const router = new express_1.Router();
-router.get('/', rootController_ts_1.get);
